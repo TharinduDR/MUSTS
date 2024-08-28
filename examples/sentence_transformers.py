e34c711e2abb99cafd9f0f0a0534d79b5950d5a0
TharinduDR <rhtdranasinghe@gmail.com>
2024-08-28 20:17:23 +0100

    001: Adding experiments

--- examples/sentence_transformers.py
+++ examples/sentence_transformers.py
@@ -6,12 +6,12 @@ from musts.run_benchmark import test
 model = SentenceTransformer("Lajavaness/bilingual-embedding-large", trust_remote_code=True).cuda()
 
 def predict(to_predict):
-    query_prompt_name = "s2s_query"
+    # query_prompt_name = "s2s_query"
     sentences_1 = list(zip(*to_predict))[0]
     sentences_2 = list(zip(*to_predict))[1]
 
-    embeddings_1 = model.encode(sentences_1, batch_size=32, show_progress_bar=True, prompt_name=query_prompt_name)
-    embeddings_2 = model.encode(sentences_2, batch_size=32, show_progress_bar=True, prompt_name=query_prompt_name)
+    embeddings_1 = model.encode(sentences_1, batch_size=32, show_progress_bar=True)
+    embeddings_2 = model.encode(sentences_2, batch_size=32, show_progress_bar=True)
 
     cosine_similarity_matrix = util.cos_sim(embeddings_1, embeddings_2)
     sims = cosine_similarity_matrix.diagonal().tolist()
