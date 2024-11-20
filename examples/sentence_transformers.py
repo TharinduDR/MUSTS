50ccc5a08c3f36637a42f158bf2a01579ee18437
TharinduDR <rhtdranasinghe@gmail.com>
2024-11-20 14:50:30 +0000

    001: Adding training script

--- examples/sentence_transformers.py
+++ examples/sentence_transformers.py
@@ -3,7 +3,7 @@
 from sentence_transformers import SentenceTransformer, util
 from musts.run_benchmark import test
 
-model = SentenceTransformer("BAAI/bge-en-icl", trust_remote_code=True).cuda()
+model = SentenceTransformer("/storage/hpc/37/ranasint/projects/MUSTS/output/training_musts_Alibaba-NLP-gte-Qwen2-1.5B-instruct/final", trust_remote_code=True).cuda()
 
 def predict(to_predict):
     # query_prompt_name = "s2s_query"
