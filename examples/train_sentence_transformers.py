6214506e5eb88b37c55439a21f2f6aef98f4fb10
TharinduDR <rhtdranasinghe@gmail.com>
2024-11-20 14:41:57 +0000

    001: Adding training script

--- examples/train_sentence_transformers.py
+++ examples/train_sentence_transformers.py
@@ -15,7 +15,7 @@ final_output_dir = ""
 
 
 def train_musts(train_df):
-    model_name = "Alibaba-NLP/gte-Qwen2-7B-instruct"
+    model_name = "Alibaba-NLP/gte-Qwen2-1.5B-instruct"
     train_batch_size = 8
     num_epochs = 5
     output_dir = (
