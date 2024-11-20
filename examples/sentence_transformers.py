6f2fe606fc295272c3d6bd1f741f347a5da1c2a6
TharinduDR <rhtdranasinghe@gmail.com>
2024-11-20 09:37:59 +0000

    001: Adding training script

--- examples/sentence_transformers.py
+++ examples/sentence_transformers.py
@@ -3,15 +3,15 @@
 from sentence_transformers import SentenceTransformer, util
 from musts.run_benchmark import test
 
-model = SentenceTransformer("nvidia/NV-Embed-v2", trust_remote_code=True).cuda()
+model = SentenceTransformer("BAAI/bge-en-icl", trust_remote_code=True).cuda()
 
 def predict(to_predict):
     # query_prompt_name = "s2s_query"
     sentences_1 = list(zip(*to_predict))[0]
     sentences_2 = list(zip(*to_predict))[1]
 
-    embeddings_1 = model.encode(sentences_1, batch_size=1, show_progress_bar=True)
-    embeddings_2 = model.encode(sentences_2, batch_size=1, show_progress_bar=True)
+    embeddings_1 = model.encode(sentences_1, batch_size=32, show_progress_bar=True)
+    embeddings_2 = model.encode(sentences_2, batch_size=32, show_progress_bar=True)
 
     cosine_similarity_matrix = util.cos_sim(embeddings_1, embeddings_2)
     sims = cosine_similarity_matrix.diagonal().tolist()
