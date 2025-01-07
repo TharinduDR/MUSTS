3e8d694978dd8e16b98518b995faf93e52360717
TharinduDR <rhtdranasinghe@gmail.com>
2025-01-07 20:22:41 +0000

    005: Modifying training arguments

---
+++ examples/smooth_inverse_frequency.py
@@ -0,0 +1,16 @@
+# To run this you need to do pip install simplests
+from simplests.model_args import WordEmbeddingSTSArgs
+from simplests.algo.sif import WordEmbeddingSIFSTSMethod
+
+from musts.run_benchmark import test
+
+
+def predict(to_predict):
+    model_args = WordEmbeddingSTSArgs()
+    model_args.embedding_models = [["transformer", "google/rembert"]]
+    model = WordEmbeddingSIFSTSMethod(model_args=model_args)
+    pred_sims = model.predict(to_predict)
+
+    return pred_sims
+
+test(predict)
